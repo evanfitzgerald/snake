05c9a4fbef7fbe49c9e17b725abf0613f1f2aa87
Evan Fitzgerald <evandfitz@gmail.com>
2019-06-23 09:04:30 -0300

    able to avoid simple inclosed spaces

--- AI.py
+++ AI.py
@@ -1,38 +1,38 @@
 
 import pygame, time, random, sys
 
-pygame.init()
-
 # settings
+pygame.init()
 height = 400
 width = 600
 display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake")
+
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 red = pygame.Color(240, 10, 10)
 green = pygame.Color(0, 255, 0)
+yellow = pygame.Color(255, 255, 0)
 background = pygame.Color(36, 158, 93)
-s = pygame.Color(185, 225, 95)
 button = pygame.Color(1, 175, 170)
 button2 = pygame.Color(1, 160, 180)
-clock = pygame.time.Clock()
-speed = 20
 
-highscore = 0
-delta = 20
-score = 0
+clock = pygame.time.Clock()
+listy = spaces = []
+highscore = score = 0
+delta = speed = 20
 snakePos = [80, 240]
 snakeBody = [[80, 240], [60, 240], [40, 240]]
 foodPos = [random.randrange(1, width // delta) * delta, random.randrange(1, height // delta) * delta]
 foodSpawn = True
-direction = ''
+direction = 'r'
 changeto = ''
 
 # when the user quits the game
 def quit():
-    pygame.event.post(pygame.event.Event(pygame.QUIT))
-    sys.exit()
+    pygame.display.quit()
+    pygame.quit()
+    exit()
 
 # when the snake is out of bounds or hits self
 def gameOver():
@@ -43,14 +43,17 @@ def gameOver():
     
 # start game
 def start():
-    global score, snakePos, snakeBody, foodPos, foodSpawn, direction, changeto
+    global score, snakePos, snakeBody, foodPos, foodSpawn, direction, changeto, listy, spaces
     score = 0
+    listy = spaces = []
     snakePos = [80, 240]
     snakeBody = [[80, 240], [60, 240], [40, 240]]
     foodPos = [random.randrange(1, width // delta) * delta, random.randrange(1, height // delta) * delta]
     foodSpawn = True
     direction = 'r'
     changeto = ''
+    board()
+    create() 
 
 # shows the score
 def showScore():
@@ -73,15 +76,47 @@ def draw():
     pygame.draw.rect(display, red, pygame.Rect(foodPos[0], foodPos[1], delta, delta))
     showScore()
 
-
 # check for out of bounds and self hits
 def check():
+    global snakeBody
     if 0 > snakePos[0] or snakePos[0] >= width or 0 > snakePos[1] or snakePos[1] >= height:
         gameOver()
 
     for block in snakeBody[1:]:
         if snakePos == block:
             gameOver()
+
+# list of all possible spaces on board
+def board():
+    global spaces
+    for i in range(30):
+        for j in range(20):
+            spaces.append([i*delta,j*delta])
+    
+# creates list of dangerous spaces
+def create():
+    global snakeBody, foodPos, spaces, listy
+
+    listy = []
+    for x in spaces:
+        check1 = check2 = check3 = check4 = False
+        for block in snakeBody[1:]:
+            if ((x[0] > block[0] and x[1] == block[1]) or (x[0] > snakePos[0] and x[1] == snakePos[1])):
+                check1 = True
+            if ((x[0] < block[0] and x[1] == block[1]) or (x[0] < snakePos[0] and x[1] == snakePos[1])):
+                check2 = True
+            if ((x[0] == block[0] and x[1] < block[1]) or (x[0] == snakePos[0]and x[1] < snakePos[1])):
+                check3 = True
+            if ((x[0] == block[0] and x[1] > block[1]) or (x[0] == snakePos[0] and x[1] > snakePos[1])):
+                check4 = True
+        if check1 == check2 == check3 == check4 == True:
+            listy.append([x[0],x[1]])
+
+    # displays dangerous spaces on board
+    #display.fill(black)
+    #for x in listy:
+        #if x != snakeBody or snakePos or foodPos:
+            #pygame.draw.rect(display, yellow, pygame.Rect(x[0], x[1], delta, delta))
     
 # pause (spacebar)
 def pause():
@@ -95,11 +130,12 @@ def pause():
                     quit()
                 if event.key == pygame.K_SPACE:
                     pause = False
+        create()
         draw()
         display.blit(pygame.image.load('images/paused.png'), (225, 125))
         pygame.display.update()
         
-# pause (spacebar)
+# menu pane
 def menu():
     menu = True
     while menu:
@@ -116,6 +152,7 @@ def menu():
         display.fill(background)
         display.blit(pygame.image.load('images/snake.png'), (60, -40)) 
         showScore()
+        # start button
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
@@ -134,24 +171,23 @@ def menu():
 
 # how it decides to move
 def move():
-    global changeto, direction
-
+    global changeto, direction, listy
     #if snake is to the right of the food
     if snakePos[0] > foodPos[0]:
         changeto = 'l'
         if direction == 'r':
             #up or down
-            if snakePos[1] > foodPos[1]:
-                changeto = 'u'
-            else: 
+            if snakePos[1] < foodPos[1]:
                 changeto = 'd'
+            else: 
+                changeto = 'u'
     
     #if snake is to the left of the food
     if snakePos[0] < foodPos[0]:
         changeto = 'r'
         if direction == 'l':
             #up or down
-            if snakePos[1] > foodPos[1]:
+            if snakePos[1] < foodPos[1]:
                 changeto = 'u'
             else: 
                 changeto = 'd'
@@ -161,7 +197,7 @@ def move():
         changeto = 'u'
         if direction == 'd':
             #left or right
-            if snakePos[0] > foodPos[0]:
+            if snakePos[0] < foodPos[0]:
                 changeto = 'l'
             else: 
                 changeto = 'r'
@@ -171,26 +207,35 @@ def move():
         changeto = 'd'
         if direction == 'u':
             #left or right
-            if snakePos[0] > foodPos[0]:
-                changeto = 'l'
-            else: 
+            if snakePos[0] < foodPos[0]:
                 changeto = 'r'
+            else: 
+                changeto = 'l'
 
     for block in snakeBody[1:]:
-
         # wants to go left but body is there
+        
         if changeto == 'l' and snakePos[0] - delta == block[0] and snakePos[1] == block[1]:
+            create()
             if direction == 'l':
                 # up or down
                 changeto = 'u'
-                if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
-                    changeto = 'd'
+                for block in snakeBody[1:]:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'd'
+                for block in listy:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'd'
                 
             if direction == 'u':
                 # up or right
-                changeto = 'r'
-                if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
-                    changeto = 'u'
+                changeto = 'u'
+                for block in snakeBody[1:]:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'r'
+                for block in listy:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'r'
 
             if direction == 'd':
                 # down or right
@@ -198,12 +243,20 @@ def move():
                 for block in snakeBody[1:]:
                     if snakePos[1] + delta == block[1] and snakePos[0] == block[0]:
                         changeto = 'r'
-                    
+                for block in listy[:]:
+                    if snakePos[1] + delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'r'
+
+        # wants to go right but body is there           
         if changeto == 'r' and snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
+            create()
             if direction == 'r':
                 # up or down 
                 changeto = 'u'
                 for block in snakeBody[1:]:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'd'
+                for block in listy:
                     if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
                         changeto = 'd'
 
@@ -213,6 +266,9 @@ def move():
                 for block in snakeBody[1:]:
                     if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
                         changeto = 'l'
+                for block in listy:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'l'
                 
             if direction == 'd':
                 # down or left
@@ -220,12 +276,20 @@ def move():
                 for block in snakeBody[1:]:
                     if snakePos[1] + delta == block[1] and snakePos[0] == block[0]:
                         changeto = 'l'
+                for block in listy:
+                    if snakePos[1] + delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'l'
 
+        # wants to go up but body is there
         if changeto == 'u' and snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+            create()
             if direction == 'u':
-                # left or right
+                # left or right 
                 changeto = 'r' 
                 for block in snakeBody[1:]:
+                    if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'l'
+                for block in listy:
                     if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
                         changeto = 'l'
 
@@ -233,8 +297,11 @@ def move():
                 # left or down
                 changeto = 'l'
                 for block in snakeBody[1:]:
-                    if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
-                        changeto = 'l'
+                    if snakePos[0] - delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'd'
+                for block in listy:
+                    if snakePos[0] - delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'd'
 
             if direction == 'r':
                 # right or down
@@ -242,21 +309,32 @@ def move():
                 for block in snakeBody[1:]:
                     if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
                         changeto = 'd'
+                for block in listy:
+                    if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'd'
 
+        # wants to go down but body is there
         if changeto == 'd' and snakePos[1] + delta == block[1] and snakePos[0] == block[0]:
+            create()
             if direction == 'd':
                 # left or right
                 changeto = 'r'
                 for block in snakeBody[1:]:
                     if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
                         changeto = 'l'
+                for block in listy:
+                    if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'l'
 
             if direction == 'l':
                 # left or up 
-                changeto = 'u'
+                changeto = 'l'
                 for block in snakeBody[1:]:
                     if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
-                        changeto = 'l'
+                        changeto = 'u'
+                for block in listy:
+                    if snakePos[1] - delta == block[1] and snakePos[0] == block[0]:
+                        changeto = 'u'
 
             if direction == 'r':
                 # right or up 
@@ -264,13 +342,15 @@ def move():
                 for block in snakeBody[1:]:
                     if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
                         changeto = 'u'
-
+                for block in listy:
+                    if snakePos[0] + delta == block[0] and snakePos[1] == block[1]:
+                        changeto = 'u'
 
 menu()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
+            quit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 quit()
@@ -279,6 +359,7 @@ while True:
 
     move()
 
+    # checking snake is not moving in opposite direction
     if changeto == 'r' and direction != 'l':
         direction = 'r'
     if changeto == 'l' and direction != 'r':
@@ -313,27 +394,13 @@ while True:
         while foodSpawn is False:
             x = False
             foodPos = [random.randrange(1, width // delta) * delta, random.randrange(1, height // delta) * delta]
-            for block in snakeBody[1:]:
+            for block in snakeBody:
                 if foodPos == block:
                     x = True
             if x == False:
                 foodSpawn = True
-
-
-                
+              
     check()
     draw()
     pygame.display.update()
     clock.tick(speed)
-'''
-
-
-
-            # way to check if the snake is running into itself...
-    for block in snakeBody[1:]:
-        if changeto == 'r':
-        #if snakePos[0] == block[0] and snakePos[1] == block[1]:
-        #if snakePos[0] + delta == block[0] and changeto == 'r':
-
-        #changeto = 'd'
-'''
